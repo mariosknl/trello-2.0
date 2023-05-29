0b08a77dfc4a1cb36e20133320e37303c3d6d0e9
Marios Kanellopoulos <mariosknl@outlook.com>
2023-05-29 12:43:36 +0300

    header component created

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import Board from "@/components/Board";
+import Header from "@/components/Header";
+
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+		<main>
+			<Header />
+			<Board />
 			<h1>Trello 2.0 AI Clone</h1>
 		</main>
 	);
---
+++ components/Board.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+
+function Board() {
+	return (
+		<DragDropContext>
+			<Droppable droppableId="board" direction="horizontal" type="column">
+				{(provided) => <div>{/* rendering all the columns */}</div>}
+			</Droppable>
+		</DragDropContext>
+	);
+}
+export default Board;
